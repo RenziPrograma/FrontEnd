1a5eb5c11946ade7c7a5292a6440eba566f7fd34
renzi <renziprograma@gmail.com>
2022-10-03 20:14:49 -0300

    App con los módulos creados

--- Renzi-Portofolio-Angular/src/app/app.module.ts
+++ Renzi-Portofolio-Angular/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProfessionalSkillsComponent } from './components/professional-skills/professional-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    ProfessionalSkillsComponent
   ],
   imports: [
     BrowserModule
